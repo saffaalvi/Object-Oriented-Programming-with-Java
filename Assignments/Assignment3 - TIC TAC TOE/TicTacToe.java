73285dafea00cb403e6f7a6fa6a010e473e33f0f
Saffa Alvi <57377830+saffaalvi@users.noreply.github.com>
2020-01-11 22:12:43 -0500

    Create TicTacToe.java

---
+++ Assignments/Assignment3 - TIC TAC TOE/TicTacToe.java
@@ -0,0 +1,14 @@
+/**
+ * Starts the new game
+ * Assignment 3
+ * December 1, 2019
+ */
+public class TicTacToe implements Global
+{
+	public static void main(String[] args)
+	{
+		Game myGame = new Game();		//instantiates myGame of type Game
+		myGame.start();					//starts the new Game
+	}
+}
+
